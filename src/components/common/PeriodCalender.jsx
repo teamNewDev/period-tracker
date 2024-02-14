26638475416602349ac5a6e8a93b8e35896c83e3
marveldev <marvellousalika48@gmail.com>
2024-02-14 12:03:05 +0100

    create and style the initialSetUpPage component - remove redundant code - add select range feature to the period calender

--- src/components/common/PeriodCalender.jsx
+++ src/components/common/PeriodCalender.jsx
@@ -1,21 +1,26 @@
 import Calendar from 'react-calendar'
+import { useState } from 'react'
 import './common.scss'
 
 const PeriodCalender = () => {
-  const datesToAddClassTo = ['2024-02-06', '2024-02-07', '2024-02-08']
+  const periodDaysInMonth = ['2024-2-6', '2024-2-7', '2024-2-8']
+  const [periodDaysRange, setPeriodDaysRange] = useState([new Date(2024, 2, 6), new Date(2024, 2, 8)])
 
-  const dateHasClass = (date) => {
-    return datesToAddClassTo.includes(date.toISOString().split('T')[0])
-  }
+  // const hasMatchingPeriodDays = (date) => {
+  //   const dateInISOString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+  //   if (periodDaysInMonth.includes(dateInISOString)) {
+  //     return true
+  //   }
+  // }
 
   return (
-    <div className="calender-wrapper">
+    <div className="period-calender">
       <Calendar
-        // value={selectedDate}
-        tileClassName={({ date, view }) => (view === 'month' && dateHasClass(date) ? 'myClassName' : null)}
+        value={periodDaysRange}
+        // tileClassName={({ date, view }) => (view === 'month' && hasMatchingPeriodDays(date) ? 'period-day' : '')}
         onChange={() => {}}
         calendarType="gregory"
-        // showNeighboringMonth={false}
+        selectRange={true}
       />
     </div>
   )
---
+++ src/components/initialSetUpPage/index.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import { PeriodCalender } from '../common'
+import './index.scss'
+
+const InitialSetUpPage = () => {
+  const [periodLog, setPeriodLog] = useState({ cycleLength: '', periodLength: '', lastPeriodDate: '2' })
+
+  return (
+    <div className="set-up-page">
+      {periodLog.cycleLength && (
+        <div className="page-wrapper">
+          <div>
+            <p className="title">How long is your average cycle?</p>
+            <span className="subtitle">If you’re irregular or not sure, tap ‘Not Sure’</span>
+          </div>
+
+          {/* <PeriodLogInput /> */}
+
+          <div className="button-wrapper">
+            <button type="button" className="not-sure-button">
+              <span>Not Sure</span>
+            </button>
+            <button type="button" className="confirm-button">
+              <span>Confirm</span>
+            </button>
+          </div>
+        </div>
+      )}
+
+      {periodLog.periodLength && (
+        <div className="set-up-page">
+          <div>
+            <p className="title">What is your average Period length?</p>
+            <span className="subtitle">If you’re irregular or not sure, tap ‘Not Sure’</span>
+          </div>
+
+          {/* <PeriodLogInput /> */}
+
+          <div className="button-wrapper">
+            <button type="button" className="not-sure-button">
+              <span>Not Sure</span>
+            </button>
+            <button type="button" className="confirm-button">
+              <span>Confirm</span>
+            </button>
+          </div>
+        </div>
+      )}
+
+      {periodLog.lastPeriodDate && (
+        <div className="set-up-page">
+          <div className="header">
+            <p className="title">When did you start your last period?</p>
+            <span className="subtitle">If you’re irregular or not sure, tap ‘Not Sure’</span>
+          </div>
+
+          <div className="calender-wrapper">
+            <PeriodCalender />
+          </div>
+
+          <div className="button-wrapper">
+            <button type="button" className="not-sure-button">
+              <span>Not Sure</span>
+            </button>
+            <button type="button" className="confirm-button">
+              <span>Confirm</span>
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default InitialSetUpPage
